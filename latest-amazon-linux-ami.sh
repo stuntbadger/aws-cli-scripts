86585e3c18a2b1bbebda188dabb5da1953098505
John Penney <john.penney@gmail.com>
2018-10-02 07:56:55 +0100

    Create latest-amazon-linux-ami.sh

---
+++ latest-amazon-linux-ami.sh
@@ -0,0 +1,9 @@
+aws ec2 describe-images \
+  --owners amazon \
+  --filters \
+    'Name=name,Values=amzn-ami-hvm-????.??.?.????????-x86_64-gp2' \
+    'Name=state,Values=available' \
+    'Name=architecture,Values=x86_64' \
+    'Name=virtualization-type,Values=hvm' \
+    'Name=root-device-type,Values=ebs' \
+  --query 'sort_by(Images, &CreationDate)[-1].ImageId'
